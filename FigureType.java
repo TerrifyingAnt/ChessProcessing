edbf311bd22ad1453e44a6ca4579de9a50837490
Ant <80172256+TerrifyingAnt@users.noreply.github.com>
2023-05-13 00:41:06 +0300

    Добавлен enum с типом фигур

---
+++ FigureType.java
@@ -0,0 +1,15 @@
+enum FigureType {
+    WHITE_PAWN,
+    WHITE_KNIGHT,
+    WHITE_BISHOP,
+    WHITE_ROCK, 
+    WHITE_QUEEN,
+    WHITE_KING,
+    BLACK_PAWN,
+    BLACK_KNIGHT,
+    BLACK_BISHOP,
+    BLACK_ROCK,
+    BLACK_KING,
+    BLACK_QUEEN, 
+    NOTHING
+}
